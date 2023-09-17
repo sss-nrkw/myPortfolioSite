3ec527bf296ffa224eff80d21094768ef4f4dac9
nari0 <narinari3579@gmail.com>
2023-09-17 22:32:16 +0900

    third

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 // pages/_app.js
 
 import '@/styles/globals.css';
-import Layout from '../components/layout';
+import Layout from 'C:/Users/nari0/iiii/myPortfolioSite/src/components/Layout.jsx';
 
 export default function App({ Component, pageProps }) {
   return (
