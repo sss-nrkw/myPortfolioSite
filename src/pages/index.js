64d26dab8359dbeca6ad69f47c8886bdbc1e710c
nari0 <narinari3579@gmail.com>
2023-09-17 21:58:56 +0900

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 // pages/index.js
 
 import Hero from '@/components/Hero';
-import About from '@/components/About';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
+
 
 import Head from 'next/head';
 
